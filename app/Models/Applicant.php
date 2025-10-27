a52ea30278b51c20db2d65b7d511afc49e387e8e
A.Galimzhan <a.galimzhan@redmadrobot.kz>
2025-10-28 00:58:33 +0500

    Feat: Add the github workflow to deploy

--- app/Models/Applicant.php
+++ app/Models/Applicant.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Applicant extends Model
 {
-    //
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable, HasUlids;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'user_id',
+    ];
 }
