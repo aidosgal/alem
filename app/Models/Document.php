ca33e699e5b772c0357af62484f03e43da0be270
A.Galimzhan <a.galimzhan@redmadrobot.kz>
2025-10-29 16:06:04 +0500

    Fix: Update the models according to migrations

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Document extends Model
 {
-    //
+    protected $fillable = [
+        'applicant_id',
+        'name',
+        'content',
+    ];
+
+    /**
+     * Get the applicant that owns the document.
+     */
+    public function applicant(): BelongsTo
+    {
+        return $this->belongsTo(Applicant::class);
+    }
 }
